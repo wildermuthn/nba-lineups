670d182bb6b77364a9aff6f114a17434d00ef62e
wildermuthn <nate.wildermuth@gmail.com>
2023-07-10 17:17:09 -0500

    basic files

---
+++ config.py
@@ -0,0 +1,13 @@
+# Path to the data
+DATA_PATH = 'data/lineup_diffs'
+
+# Batch size for the DataLoader
+BATCH_SIZE = 32
+
+# Model parameters
+MODEL_PARAMS = {
+    'embedding_dim': 128,
+    'num_layers': 3,
+    'dropout': 0.1,
+    # Add any other parameters your model needs
+}
