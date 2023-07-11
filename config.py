a080fe78fa3fdfce2b180010e722a899a882258b
wildermuthn <nate.wildermuth@gmail.com>
2023-07-11 02:16:40 -0500

    best config so far

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 DATA_PATH = 'data/raw'
 
 # Batch size for the DataLoader
-BATCH_SIZE = 512
+BATCH_SIZE = 64
 
 # Model parameters
 MODEL_PARAMS = {
     'lr': 0.0001,
     'batch_size': BATCH_SIZE,
-    'linear_embedding_dim': 512,
-    'player_embedding_dim': 512,
+    'linear_embedding_dim': 32,
+    'player_embedding_dim': 32,
     # Add any other parameters your model needs
 }
 
