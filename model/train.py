fc585bdff13a663932522c6c32c802bb271478d8
wildermuthn <nate.wildermuth@gmail.com>
2023-07-13 15:37:30 -0500

    wip

--- model/train.py
+++ model/train.py
@@ -2,13 +2,9 @@ import torch
 import wandb
 
 def train_loop(dataloader, model, loss_fn, optimizer, epoch):
-    size = len(dataloader.dataset)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # Set the model to training mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
     model.train()
     for batch, (X, y) in enumerate(dataloader):
-        n_samples = (batch+1 * len(X)) + (len(X) * len(dataloader) * epoch)
         X = X.to(device)
         y = y.float().to(device)
         pred = model(X)
@@ -19,5 +15,5 @@ def train_loop(dataloader, model, loss_fn, optimizer, epoch):
         optimizer.step()
         optimizer.zero_grad()
         wandb.log({"train_loss": loss.item(),
-                   "n_samples": n_samples
+                   "samples": epoch * len(dataloader.dataset) + batch * len(X)
                    })
