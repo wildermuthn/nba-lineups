746c05c0fe1c8e0b77184b00a62182c513cc47d6
wildermuthn <nate.wildermuth@gmail.com>
2023-07-14 16:56:59 -0500

    wip

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ BATCH_SIZE = 12000
 MODEL_PARAMS = {
     'lr': 0.001,
     'batch_size': BATCH_SIZE,
-    'linear_embedding_dim': 128,
-    'player_embedding_dim': 128,
+    'linear_embedding_dim': 12,
+    'player_embedding_dim': 12,
     'model': 'LineupPredictorTransformer',
     'n_head': 4,
     'n_layers': 2,
