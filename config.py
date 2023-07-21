9582b1e3cc994e44794edab5202ca5aa188939ee
wildermuthn <nate.wildermuth@gmail.com>
2023-07-20 20:51:30 -0500

    wip

--- config.py
+++ config.py
@@ -2,19 +2,19 @@
 DATA_PATH = 'data/raw'
 
 # Batch size for the DataLoader
-BATCH_SIZE = 24000
+BATCH_SIZE = 12000
 
-EPOCHS_PER_CHECKPOINT = 500
+EPOCHS_PER_CHECKPOINT = 100
 
 # Model parameters
 MODEL_PARAMS = {
-    'lr': 0.00001,
+    'lr': 0.0001,
     'batch_size': BATCH_SIZE,
-    'linear_embedding_dim': 12,
-    'player_embedding_dim': 12,
+    'linear_embedding_dim': 64,
+    'player_embedding_dim': 64,
     'model': 'LineupPredictorTransformer',
-    'n_head': 4,
-    'n_layers': 2,
+    'n_head': 8,
+    'n_layers': 4,
     'optimizer': 'Adam',
     'lineup_time_played_threshold': 90,
     'lineup_abs_point_max_threshold_per_60': 10,
@@ -22,7 +22,7 @@ MODEL_PARAMS = {
     'min_max_target': True,
     'z_score_target': False,
     'gradient_clipping': False,
-    'transformer_dropout': 0,
+    'transformer_dropout': 0.5,
     'xavier_init': True,
     # 'specific_init': 50.0
     # Add any other parameters your model needs
--- run.py
+++ run.py
@@ -214,10 +214,10 @@ def eval(filepath=None):
         player_id_age = dataset.get_player_tensor_indexes(player, 0)
         player_id_age = torch.tensor(player_id_age).to(device)
         generic_players[0][0] = player_id_age
-        generic_players[0][1] = player_id_age
-        generic_players[0][2] = player_id_age
-        generic_players[0][3] = player_id_age
-        generic_players[0][4] = player_id_age
+        # generic_players[0][1] = player_id_age
+        # generic_players[0][2] = player_id_age
+        # generic_players[0][3] = player_id_age
+        # generic_players[0][4] = player_id_age
         # generic_players[0][5] = player_id_age
         # generic_players[0][6] = player_id_age
         # generic_players[0][7] = player_id_age
@@ -242,5 +242,5 @@ def eval(filepath=None):
 
 
 if __name__ == "__main__":
-    # main()
-    eval('checkpoints/solar-forest-261__3000.pth')
+    main()
+    # eval('checkpoints/solar-forest-261__3000.pth')
