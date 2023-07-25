45ce33fad1ec5d27c50273297edf7454b832abd9
wildermuthn <nate.wildermuth@gmail.com>
2023-07-25 18:21:42 -0500

    wip

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 PARAMS = {
     # Meta parameters
-    'batch_size': 512,
+    'batch_size': 4096,
     'n_epochs': 1000000,
     'epochs_per_checkpoint': 100,
     'data_path': 'data/raw',
 
     # Model parameters
-    'lr': 2.7510162796840593e-05,
-    'linear_embedding_dim': 32,
-    'player_embedding_dim': 32,
-    'model': 'LineupPredictorTransformer',
-    'n_head': 8,
+    'lr': .000343,
+    'player_embedding_dim': 16,
     'n_layers': 16,
+    'n_head': 2,
+    'transformer_dropout': 0.1824,
+    'xavier_init': True,
+    'model': 'LineupPredictorTransformer',
     'optimizer': 'Adam',
     'lineup_time_played_threshold': 30,
     'lineup_abs_point_max_threshold_per_60': 30,
@@ -19,14 +20,13 @@ PARAMS = {
     'min_max_target': True,
     'z_score_target': False,
     'gradient_clipping': False,
-    'transformer_dropout': 0.21036599520792046,
-    'xavier_init': False,
     'augment_with_generic_players': False,
     'augment_every_n_samples': 100,
     'train_specific_season': None,
     'specific_init': None,
     'log_scores': True,
     'log_all': True,
+    'linear_embedding_dim': 16,
     # Add any other parameters your model needs
 }
 
