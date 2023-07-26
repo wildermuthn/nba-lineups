946514638d195f0c7b1872b97669db26d9ae00db
wildermuthn <nate.wildermuth@gmail.com>
2023-07-26 00:33:03 -0500

    wip

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ PARAMS = {
     'data_path': 'data/raw',
 
     # Model parameters
-    'lr': .01,
+    'lr': .000343,
     'player_embedding_dim': 16,
-    'n_layers': 2,
-    'n_head': 4,
-    'transformer_dropout': 0.0,
+    'n_layers': 16,
+    'n_head': 2,
+    'transformer_dropout': 0.1824,
     'xavier_init': True,
-    'model': 'LineupPredictorTransformerV2',
+    'model': 'LineupPredictorTransformer',
     'optimizer': 'Adam',
     'lineup_time_played_threshold': 30,
     'lineup_abs_point_max_threshold_per_60': 5.5,
@@ -26,8 +26,8 @@ PARAMS = {
     'augment_every_n_samples': 100,
     'train_specific_season': None,
     'specific_init': None,
-    'log_scores': True,
-    'log_all': True,
+    'log_scores': False,
+    'log_all': False,
     'linear_embedding_dim': 16,
     # Add any other parameters your model needs
 }
